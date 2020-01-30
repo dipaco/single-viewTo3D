9454d50a1b49683cab2d5ab035c41ee68b3f3678
Diego Patino <dipaco@gmail.com>
2020-01-30 15:24:21 -0500

    Revert "saving the global step"
    This reverts commit 06510113b8fc018bf6bc49c8b769415d93066507.

--- p2m/models.py
+++ p2m/models.py
@@ -120,8 +120,7 @@ class Model(object):
         if not sess:
             raise AttributeError("TensorFlow session not provided.")
         saver = tf.train.Saver(self.vars)
-        checkpoint_path = os.path.join(self.save_dir, f"checkpoint/{self.name}.ckpt")
-        save_path = saver.save(sess, checkpoint_path, global_step=self.global_step)
+        save_path = saver.save(sess, os.path.join(self.save_dir, f"checkpoint/{self.name}.ckpt"))
         print(("Model saved in file: %s" % save_path))
 
     def load(self, sess=None):
